842780f6de273f0a9a0ff0052d2c142d50ac1ddd
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2022-12-31 11:25:23 +0530

    rename index.html , add insert success page,  add data base connect to list item
    rename index.html , add listing insert success page,  add php to connect ebuy data base and update listing table

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 $(".add-card").click(function () {
   window.open("listing-page.html", "_self");
 });
+
+$("#listing-cancel").click(function () {
+  window.open("admin-portal.html", "_self");
+});
--- connect.php
+++ connect.php
@@ -3,7 +3,5 @@
     $password = '';
     $db = 'ebuy';
 
-    $db = new mysqli('localhost',$user,$password,$db) or die("Unable to connect");
-
-    echo"Great work! It's done!!!";
+    $conn = new mysqli('localhost',$user,$password,$db) or die("Unable to connect");
 ?>
