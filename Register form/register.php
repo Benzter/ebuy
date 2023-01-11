16af360ac97d59902aeb884361beb8996c5cf8f3
Dhanushka Nuwan <97797596+Benzter@users.noreply.github.com>
2023-01-11 11:13:45 +0530

    Update register.php

--- Register form/register.php
+++ Register form/register.php
@@ -1,5 +1,5 @@
 <?php
-    include 'connection.php';
+    include 'connect.php';
 
 ?>
 
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>register</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/register-style.css">
 </head>
 <body>
 
